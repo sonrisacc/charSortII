2a721106fcf6d5b9921943259817656c8c247419
Sonrisa <sonrisa722611@gmail.com>
2018-01-25 14:05:54 -0800

    Update memoize with promise

--- src/helper/util.js
+++ src/helper/util.js
@@ -11,4 +11,12 @@ const postRequest = string => {
     .catch(error => console.error('Error:', error));
 };
 
-module.exports = postRequest;
+const test = a => {
+  return a + 3;
+};
+
+console.log(memoize(1).then(res => console.log(res)));
+console.log(memoize(2).then(res => console.log(res)));
+console.log(memoize(3).then(res => console.log(res)));
+console.log(memoize(1).then(res => console.log(res)));
+// module.exports = postRequest;
